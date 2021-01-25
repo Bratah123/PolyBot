e81075907e417578819e181c7e70f6ac797d8da3
KOOKIIE <25145447+KOOKIIEStudios@users.noreply.github.com>
2021-01-26 07:49:08 +0800

    FIX: Bugs caused by typos
    Also made the bot round off to 2dp

--- src/commands.py
+++ src/commands.py
@@ -291,7 +291,7 @@ class Commands(commands.Cog, name="commands"):
         # Takes in args like km, m, cm, mi, ft, in
         unit_from = args[1].lower()
         unit_to = args[2].lower()
-        source_value = int(args[3])
+        source_value = float(args[3])
 
         # SI to Imperial
         if unit_from in utility.SI_UNITS:  # do conversions as km-mi
@@ -299,21 +299,21 @@ class Commands(commands.Cog, name="commands"):
             source_in_mi = source_in_km * 0.621371192
             if unit_to == "mi":
                 output = source_in_mi
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             elif unit_to == "ft":
                 output_ft_component = source_in_mi * 5280
                 if not int(output_ft_component):
                     # if ft component < 1
-                    await ctx.send(f"{source_value}{unit_from} = {source_in_mi * 5280}{unit_to}")
+                    await ctx.send(f"{source_value}{unit_from} = {output_ft_component:.2f}{unit_to} (2dp)")
                     return
                 else:
                     output_in_component = (output_ft_component - int(output_ft_component)) * 12
-                    await ctx.send(f"{source_value}{unit_from} = {int(output_ft_component)}{unit_to} {output_in_component}in")
+                    await ctx.send(f"{source_value}{unit_from} = {int(output_ft_component)}{unit_to} {output_in_component:.2f}in (2dp)")
                     return
             elif unit_to == "in":
                 output = source_in_mi * 63360
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             else:
                 await ctx.send("Invalid units!")
@@ -325,31 +325,31 @@ class Commands(commands.Cog, name="commands"):
             source_in_km = source_in_mi / 0.621371192
             if unit_to == "km":
                 output = source_in_km
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             elif unit_to == "m":
                 output_m_component = source_in_km * 1000
                 if not int(output_m_component):
                     # if m component < 1
-                    await ctx.send(f"{source_value}{unit_from} = {output_m_component}{unit_to}")
+                    await ctx.send(f"{source_value}{unit_from} = {output_m_component:.2f}{unit_to} (2dp)")
                     return
                 else:
                     output_cm_component = (output_m_component - int(output_m_component)) * 100
-                    await ctx.send(f"{source_value}{unit_from} = {int(output_m_component)}{unit_to} {output_cm_component}cm")
+                    await ctx.send(f"{source_value}{unit_from} = {int(output_m_component)}{unit_to} {output_cm_component:.2f}cm (2dp)")
                     return
             elif unit_to == "cm":
                 output_cm_component = source_in_km * 100000
                 if not int(output_cm_component):
                     # if cm component < 1
-                    await ctx.send(f"{source_value}{unit_from} = {output_cm_component}{unit_to}")
+                    await ctx.send(f"{source_value}{unit_from} = {output_cm_component:.2f}{unit_to} (2dp)")
                     return
                 else:
                     output_mm_component = (output_cm_component - int(output_cm_component)) * 10
-                    await ctx.send(f"{source_value}{unit_from} = {int(output_cm_component)}{unit_to} {output_mm_component}cm")
+                    await ctx.send(f"{source_value}{unit_from} = {int(output_cm_component)}{unit_to} {output_mm_component:.2f}cm (2dp)")
                     return
             elif unit_to == "mm":
                 output = source_in_km * 1000000
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             else:
                 await ctx.send("Invalid units!")
@@ -367,7 +367,7 @@ class Commands(commands.Cog, name="commands"):
         # Takes in args like kg, g, lbs, oz
         unit_from = args[1].lower()
         unit_to = args[2].lower()
-        source_value = int(args[3])
+        source_value = float(args[3])
 
         # SI to Imperial
         if unit_from in utility.SI_UNITS:  # do conversions as kg-lbs
@@ -375,11 +375,11 @@ class Commands(commands.Cog, name="commands"):
             source_in_lbs = source_in_kg / 0.45359237
             if unit_to == "lbs":
                 output = source_in_lbs
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             elif unit_to == "oz":
                 output = source_in_lbs * 16
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             else:
                 await ctx.send("Invalid units!")
@@ -391,11 +391,11 @@ class Commands(commands.Cog, name="commands"):
             source_in_kg = source_in_lbs * 0.45359237
             if unit_to == "kg":
                 output = source_in_kg
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             elif unit_to == "g":
                 output = source_in_kg * 1000
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             else:
                 await ctx.send("Invalid units!")
@@ -413,7 +413,7 @@ class Commands(commands.Cog, name="commands"):
         # Takes in args like ml, l, oz, pint, gallon
         unit_from = args[1].lower()
         unit_to = args[2].lower()
-        source_value = int(args[3])
+        source_value = float(args[3])
 
         # SI to Imperial
         if unit_from in utility.SI_UNITS:  # do conversions as ml-oz
@@ -421,15 +421,15 @@ class Commands(commands.Cog, name="commands"):
             source_in_oz = source_in_ml / 29.57
             if unit_to == "oz":
                 output = source_in_oz
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             elif unit_to == "pint":
                 output = source_in_oz / 16
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             elif unit_to == "gallon":
                 output = source_in_oz / 128
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             else:
                 await ctx.send("Invalid units!")
@@ -441,11 +441,11 @@ class Commands(commands.Cog, name="commands"):
             source_in_ml = source_in_oz * 0.45359237
             if unit_to in ("ml", "cc"):
                 output = source_in_ml
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             elif unit_to == "l":
                 output = source_in_ml / 1000
-                await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+                await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
                 return
             else:
                 await ctx.send("Invalid units!")
@@ -463,29 +463,29 @@ class Commands(commands.Cog, name="commands"):
         # Takes in args like c, f, k
         unit_from = args[1].lower()
         unit_to = args[2].lower()
-        source_value = int(args[3])
+        source_value = float(args[3])
 
         # 3P2 = 6 possible permutations
         if unit_from == "c" and unit_to == "k":
             output = utility.c_to_k(source_value)
-            await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+            await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
         elif unit_from == "k" and unit_to == "c":
             output = utility.k_to_c(source_value)
-            await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+            await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
         elif unit_from == "f" and unit_to == "k":
             output = utility.f_to_k(source_value)
-            await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+            await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
         elif unit_from == "k" and unit_to == "f":
             output = utility.k_to_f(source_value)
-            await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+            await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
         elif unit_from == "f" and unit_to == "c":
             output = utility.f_to_k(source_value)
             output = utility.k_to_c(output)
-            await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+            await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
         elif unit_from == "c" and unit_to == "f":
             output = utility.c_to_k(source_value)
             output = utility.k_to_f(output)
-            await ctx.send(f"{source_value}{unit_from} = {output}{unit_to}")
+            await ctx.send(f"{source_value}{unit_from} = {output:.2f}{unit_to} (2dp)")
         else:
             # Sanity-check:
             await ctx.send("Invalid units!")

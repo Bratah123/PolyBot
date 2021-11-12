8a655b765e77c3b9e0550be1dcb885802a598960
KOOKIIE <25145447+KOOKIIEStudios@users.noreply.github.com>
2021-11-12 15:55:48 +0800

    REFACTOR: (main) Make help & credit text constant
    Reduce the workload of the help/credit commands by refactoring some of the logic out.

--- src/main.py
+++ src/main.py
@@ -5,47 +5,22 @@ bot = commands.Bot(command_prefix='!')
 
 print("Loading all commands...")
 
-# We remove the help command because we want to add our own fancy one!
+# Remove the default help command:
 bot.remove_command('help')
-
-# load the class in "commands.py"
+# Load the class in "commands.py":
 bot.load_extension("commands")
-print("Finished loading commands!")
 
 
-@bot.event
-async def on_ready():
-    print("Bot is now online!")  # Print this when the bot is online
-
-
-@bot.command(name='help', pass_context=True)  # Here is our own help command we add
-async def help_command(ctx):
-    args = ctx.message.content.split(" ")
-    if len(args) > 2:  # Reject odd formats - Added by KOOKIIE
-        await ctx.send(
-            "The format of !help is either '!help' or '!help <command>'"
-        )
-
-    command_cog = bot.get_cog("commands")
+# Helper methods for loading custom help & credit command:
+def get_commands():
     # Get list of command names & briefs (ordered pairs)
+    command_cog = bot.get_cog("commands")
     cmd_list = [command.name for command in command_cog.get_commands()]
     brief_list = [command.brief for command in command_cog.get_commands()]
+    return cmd_list, brief_list
 
-    # Give description of command - Added by KOOKIIE
-    # Check if it's of the format "!help <command>"
-    # short-circuits if there's 1 arg after "!help"
-    if len(args) == 2:
-        if args[1] in cmd_list:
-            await ctx.send(  # map name to brief:
-                f"**!{args[1]}**: {brief_list[cmd_list.index(args[1])]}"
-            )
-            # For "!help translate", it sends:
-            # !**Translate**: Translates messages between 2 different languages.
-            return
-        else:
-            await ctx.send(f"Could not find the command '{args[1]}'")
-            return
 
+def format_help_command_text(cmd_list):
     # Flatten command-name list to String, to add to embed
     cmd_list_str = "\n".join(cmd_list)
     detailed = (
@@ -63,13 +38,10 @@ async def help_command(ctx):
     embed_msg.add_field(name="User Commands", value=cmd_list_str, inline=False)
     embed_msg.add_field(name="Additional Options", value=detailed, inline=False)
     embed_msg.set_footer(text="PolyBot")
+    return embed_msg
 
-    await ctx.send(embed=embed_msg)
 
-
-@bot.command(name='credit', pass_context=True)  # Here is our own help command we add
-async def credit_command(ctx):
-    # Boiler plate embed message builder
+def initialise_credit_text():
     embed_msg = discord.Embed(
         title="About",
         description="PolyBot is an all-in-one utility bot created by Brandon",
@@ -82,7 +54,52 @@ async def credit_command(ctx):
     )
     embed_msg.add_field(name="Links", value=links, inline=False)
     embed_msg.set_footer(text="PolyBot")
-    await ctx.send(embed=embed_msg)
+    return embed_msg
+
+
+# Load custom help command
+COMMANDS, COMMAND_DETAILS = get_commands()
+MAIN_HELP_TEXT = format_help_command_text(COMMANDS)
+# Load credit text
+CREDIT_TEXT = initialise_credit_text()
+print("Finished loading commands!")
+
+
+@bot.event
+async def on_ready():
+    print("Bot is now online!")  # Print this when the bot is online
+
+
+@bot.command(name='help', pass_context=True)  # Here is our own help command we add
+async def help_command(ctx):
+    args = ctx.message.content.split(" ")
+    if len(args) > 2:  # (Sanity check) Reject odd formats - Added by KOOKIIE
+        await ctx.send(
+            "The format of !help is either '!help' or '!help <command>'"
+        )
+        return
+
+    # Give description of command - Added by KOOKIIE
+    # Check if it's of the format "!help <command>"
+    # short-circuits if there's 1 arg after "!help"
+    if len(args) == 2:
+        if args[1] in COMMANDS:
+            await ctx.send(  # map name to brief:
+                f"**!{args[1]}**: {COMMAND_DETAILS[COMMANDS.index(args[1])]}"
+            )
+            # E.g.: For "!help translate", it sends:
+            # !**Translate**: Translates messages between 2 different languages.
+            return
+        else:
+            await ctx.send(f"Could not find the command `!{args[1]}`")
+            return
+
+    await ctx.send(embed=MAIN_HELP_TEXT)
+
+
+@bot.command(name='credit', pass_context=True)  # Here is our own help command we add
+async def credit_command(ctx):
+    await ctx.send(embed=CREDIT_TEXT)
 
 
 if __name__ == '__main__':
